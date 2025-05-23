6f3246e7f2dda64358a93ec00aa75b4f79895a49
user <user@users-MacBook-Pro.local>
2025-05-23 15:40:39 +0100

    more update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import GameBoard from "./components/Gameboard"
+import Log from "./components/Log";
 import Player from "./components/Player"
+import { useState } from "react"
+
+
 function App() {
-  
+  const [gameTurns, setGameTurns] = useState([]);
+  const [ activePlayer, setActivePlayer] =useState('X');
+
+  function handleSelectedSquare(rowIndex, colIndex){
+    setActivePlayer((currentlyActivePlayer) => currentlyActivePlayer === 'X' ? 'O' :'X' );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = 'X';
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer =' O'
+      }
+      const updatedTurns = [{ square:{row: rowIndex, col: colIndex}, player: activePlayer}, ...prevTurns]
+      return updatedTurns;
+
+    });
+  }
+
 
   return (
    <main>
     <div id="game-container">
-      <ol id="players">
+      <ol id="players" className="highlight-player">
         
-        <Player initialName="Player 1" symbol="X"/>
-        <Player initialName="Player 2" symbol="O"/>
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       
       </ol>
 
-      <GameBoard/>
+      <GameBoard
+       onSelectSquare={handleSelectedSquare} 
+       turns={gameTurns}/>
 
 
     </div>
+    <Log turns={gameTurns}/>
    </main>
   )
 }
